e77982dfa15f7543d9b358c933b937259af8ec8e
M.M <89016998+ewave33@users.noreply.github.com>
2024-02-07 09:22:49 +0000

    test 2

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* Any other Next.js config */
-  }
-  
-  module.exports = {
-    ...nextConfig,
-    basePath: process.env.NEXT_PUBLIC_BASE_URL
-  }
+const nextConfig = {};
+
+module.exports = nextConfig;
